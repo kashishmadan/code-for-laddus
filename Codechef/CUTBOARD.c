c1bd4ac9d0b434704b2f54f667d6fe4a76773401
Kashish Madan <kashish.madan@outlook.com>
2020-04-06 23:50:02 +0530

    Create CUTBOARD.c

---
+++ Codechef/CUTBOARD.c
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n,m;
+	    cin >> n >> m;
+	    cout<< (n-1)*(m-1) << endl;
+	}
+	return 0;
+}
