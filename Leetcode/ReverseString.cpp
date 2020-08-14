10135afbd7c1f12490cddb43037bb69da8a7aa45
Kashish Madan <kashish.madan@outlook.com>
2020-08-14 21:32:06 +0530

    Create ReverseString.cpp

---
+++ Leetcode/ReverseString.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        for(int i=0,j=n-1;i<n/2;i++,j--)
+        {
+            swap(s[i],s[j]);
+        }
+    }
+};
